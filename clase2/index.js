c74e87b40d24a5a16d41e84f56695f487afca52a
edwinmgallego <ing.edwin.gallego@gmail.com>
2021-11-16 14:32:03 -0500

    identificando aprendiendo variables prueba

--- clase2/index.js
+++ clase2/index.js
@@ -1,8 +1,15 @@
 //esto es un comentario
 //variables  numerico
-var  caja =5;
+var  numero =5;
 //variable de texto
 var cajadetexto ='hola';
 // variable  boleana
 var cajaVerdaderofalso = flase ;
+
+//variables  arrays o arreglos de variables
+
+var semana = ['lunes','martes','miercoles','jueves','sabado','domingo'];
+
+console.log(cajadetexto);
+console.log(semana[1]);
 

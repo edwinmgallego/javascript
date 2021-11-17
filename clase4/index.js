d23cf5b56bc043b03f68b1cbf62593927be839db
edwinmgallego <ing.edwin.gallego@gmail.com>
2021-11-17 17:27:55 -0500

    continuacion de let

--- clase4/index.js
+++ clase4/index.js
@@ -1,10 +1,12 @@
 'use strict'
 var numero =20;
+var texto="hola";
 
 
 if(true){
     let numero = 40;
     console.log(numero);
-
-}
+    console.log(texto);
+    }
 console.log(numero);
+console.log(texto);
